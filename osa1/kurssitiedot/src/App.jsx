f728b8e3f44f86d4fe0d86c13353dcae3dc6e23d
Juha Kamppuri <jkamppur@iki.fi>
2024-05-01 23:18:50 +0300

    harj 1.3 ready

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,11 +1,20 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => {
     return (
@@ -26,9 +35,9 @@ const App = () => {
   const Content = (props) => {
     return (
       <div>
-        <Part name={props.part1} exercises={props.exer1} />
-        <Part name={props.part2} exercises={props.exer2} />
-        <Part name={props.part3} exercises={props.exer3} />
+        <Part name={props.part1.name} exercises={props.part1.exercises} />
+        <Part name={props.part2.name} exercises={props.part2.exercises} />
+        <Part name={props.part3.name} exercises={props.part3.exercises} />
       </div>
     )
   }
@@ -44,8 +53,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exer1={exercises1} part2={part2} exer2={exercises2} part3={part3} exer3={exercises3}/>
-      <Total exercises={exercises1 + exercises2 + exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total exercises={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
