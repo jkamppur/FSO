e7fbbfc9ee9a2b14a73840903495cf8ce4e22a2a
Juha Kamppuri <jkamppur@iki.fi>
2024-07-13 00:15:01 +0300

    harj 1.6 ready

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <h2>statistics</h2>
+
+      <div>Good {good}</div>
+      <div>Neutral {neutral}</div>
+      <div>Bad {bad}</div>
+    </div>
+  )
+}
+
+export default App
