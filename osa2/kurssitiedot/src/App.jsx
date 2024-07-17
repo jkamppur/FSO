131577b5970c29269f8ce59d56379c8b8ba04611
Juha Kamppuri <jkamppur@iki.fi>
2024-07-17 11:14:43 +0300

    teht 2.4 ready

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,30 +1,55 @@
+// Teht 2.4
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   const Header = ({header}) => {
     return (
       <div>
-        <h2> {header} </h2>
+        <h3> {header} </h3>
       </div>
     )
   }
@@ -70,7 +95,10 @@ const App = () => {
 
   return (
     <div>
-      <Course course={course} />
+      <h2>Web development curriculum</h2>
+      {courses.map(course => 
+        <Course key={course.id} course={course}/>
+      )}
     </div>
   )
 }
