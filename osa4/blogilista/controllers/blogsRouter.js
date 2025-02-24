22fbc41fe1fbab52266a9f0e33279e58c8efbbd2
Juha Kamppuri <jkamppur@iki.fi>
2025-02-24 23:51:22 +0200

    teht 4.8 ready

--- osa4/blogilista/controllers/blogsRouter.js
+++ osa4/blogilista/controllers/blogsRouter.js
@@ -2,23 +2,17 @@ const blogsRouter = require('express').Router()
 const logger = require('../utils/logger.js')
 const Blog = require('../models/blog.js')
 
-blogsRouter.get('', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('', (request, response) => {
+blogsRouter.post('', async (request, response) => {
   logger.info(request.body)
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 module.exports = blogsRouter
